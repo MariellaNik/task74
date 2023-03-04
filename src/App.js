7c8840757f5b29dce9f335b5d8db992bbdb0fdb2
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-03-04 16:23:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import "./App.css";
 
-function App() {
+ffunction App() {
+  const [text, setText] = useState("");
+
+  const calculate = (text) => {
+    const validRegex = new RegExp(/[1-9]/);
+
+    if (validRegex.test(text)) {
+      if (text < 15) {
+        return text;
+      }
+    } else {
+      return false;
+    }
+  };
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const memoCalculate = useMemo(() => calculate(text), [text]);
+  useEffect(() => {
+    console.log(memoCalculate, text);
+  }, [memoCalculate, text]);
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          onChange={onChange}
+          value={text}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={`${memoCalculate ? "fas fa-check" : "fas fa-times"}`} />
+        </span>
       </div>
     </div>
   );
